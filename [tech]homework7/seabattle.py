3e3817ed1176f85d4b8c0f07cc8f1a9beed621fc
WirKekc <bk9419@yandex.ru>
2019-10-30 18:30:58 +0300

    add classes Feild, Ship, Shoot

--- [tech]homework7/seabattle.py
+++ [tech]homework7/seabattle.py
@@ -1,4 +1,24 @@
 class Player(object):
-    def __init__(self, name, tern):
+    def __init__(self, name, turn):
         self.name = name
-        self.tern = tern
+        self.turn = turn
+
+
+class Field(object):
+    def __init__(self, size, ships_number):
+        self.size = size
+        self.ships_number = ships_number
+
+
+class Ship(object):
+    def __init__(self, length, field_place):
+        self.length = length
+        self.field_place = field_place
+
+
+class Shoot(object):
+    def __init__(self, shooter, coordinate, is_hit):
+        self.shooter = shooter
+        self.coordinte = coordinate
+        self.is_hit = is_hit
+
