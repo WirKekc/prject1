2d8e8988376c7e55233510cad5e350f9831ad77c
WirKekc <bk9419@yandex.ru>
2019-12-02 22:26:09 +0300

    add and del 'x'

--- gameOfLife.py
+++ gameOfLife.py
@@ -1,31 +1,60 @@
 n1, n2 = map(int, input().split(' '))
 field = [input() for i in range(n1)]
+# field = []
+# a = []
+# for i in range(n1):
+#     a.append(input())
+#     for j in range(n2):
+#         field.append(a[i][j])
+changed_field = field
 
-# field = [['.' for i in range(n1)] for j in range(n2)]
+
+def print_field(f):
+    [print(*f[i]) for i in range(n1)]
 
 
-def print_field():
-    [print(*field[i]) for i in range(n1)]
-k = 0
 for i in range(len(field)):
     for j in range(len(field[i])):
+        k = 0
         if field[i][j] == 'X':
             if field[i][j-1] == 'X':
-                k+=1
+                k += 1
+            if field[i][(j+1)%n2] == 'X':
+                k += 1
+            if field[i-1][(j+1)%n2] == 'X':
+                k += 1
+            if field[i-1][j] == 'X':
+                k += 1
+            if field[i-1][j-1] == 'X':
+                k += 1
+            if field[(i+1)%n1][(j+1)%n2] == 'X':
+                k += 1
+            if field[(i+1)%n1][j] == 'X':
+                k += 1
+            if field[(i+1)%n1][j-1] == 'X':
+                k += 1
+            if k < 2 or k > 3:
+                changed_field[i] = changed_field[i][:j] + '.' + field[i][j+1:]
+        elif field[i][j] == '.':
+            if field[i][j-1] == 'X':
+                k += 1
             elif field[i][(j+1)%n2] == 'X':
-                k+=1
+                k += 1
             elif field[i-1][(j+1)%n2] == 'X':
-                k+=1
+                k += 1
             elif field[i-1][j] == 'X':
-                k+=1
+                k += 1
             elif field[i-1][j-1] == 'X':
-                k+=1
+                k += 1
             elif field[(i+1)%n1][(j+1)%n2] == 'X':
-                k+=1
+                k += 1
             elif field[(i+1)%n1][j] == 'X':
-                k+=1
+                k += 1
             elif field[(i+1)%n1][j-1] == 'X':
-                k+=1
-        if
-print_field()
+                k += 1
+            if k == 3:
+                changed_field[i] = changed_field[i][:j] + 'X' + field[i][j + 1:]
+print_field(field)
+print('-------------------')
+print_field(changed_field)
 print(field)
