75cccbeedd0f837979c5bf5c8c02dbc87d49dabc
WirKekc <bk9419@yandex.ru>
2019-12-06 14:14:41 +0300

    make graphic for koch

--- koch_curve.py
+++ koch_curve.py
@@ -1,9 +1,11 @@
-# import turtle
-num = int(input())
+import turtle
+
+# my_turtle = turtle.Turtle()
+# window = turtle.Screen()
+# num = int(input())
 
 
-def koch(n):
-    a = []
+def koch(n, a=[]):
     if n > 0:
         n -= 1
         koch(n)
@@ -15,13 +17,35 @@ def koch(n):
         koch(n)
     return a
 
-# def turtle_koch_curve(n, line_length=10):
-#     for move in koch(n):
-#         turtle.forward(line_length)
-#         turtle.left(move)
-#     turtle.forward(line_length)
-#
-#
-# turtle_koch_curve(num)
 
-print(koch(num))
+def turtle_koch_curve(n, speed, sw, sh):
+    window = turtle.Screen()  # создали окно
+    window.title('Koch curve')  # назвали
+    # window.screensize(sw, sh)  # задали размер
+
+    mikey = turtle.Turtle()  # создали черепаху
+    mikey.screen.setup(sw, sh)
+    mikey.speed(speed)  # скорость
+    mikey.up()
+    mikey.setpos(-sw // 2, -sh // 2)  # позиция
+    mikey.down()
+
+    # посчитали необходимую длину линии
+    length = sw * 3//10
+    for _ in range(n - 1):
+        length /= 3
+
+    for move in koch(n):
+        mikey.forward(length)
+        mikey.left(move)
+    mikey.forward(length)
+
+    window.exitonclick()
+
+
+num = int(input('Deep of Koch: '))
+sp = int(input('Speed of drawing: '))
+width = int(input('Width of window: '))
+height = int(input('Height of window: '))
+
+turtle_koch_curve(num, sp, width, height)  # запускаем функцию
