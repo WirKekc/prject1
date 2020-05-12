4ab072e8b2f23970441bbe70f62859c0e770773c
WirKekc <bk9419@yandex.ru>
2020-05-12 17:03:42 +0300

    rename dirs. change tech to tceh

--- [tceh]lesson9/homework9.py
+++ [tceh]lesson9/homework9.py
@@ -58,4 +58,4 @@
 
 import os
 print(os.getcwd())
-print('/home/dmitry/PycharmProjects/project1/[tech]lesson9'==os.getcwd())
+print('/home/dmitry/PycharmProjects/project1/[tceh]lesson9' == os.getcwd())
